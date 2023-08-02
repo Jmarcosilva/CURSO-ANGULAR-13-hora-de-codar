a1113907c08ca194943b1821efca9ed6c5734bcb
Jmarcosilva <marcuz.silva@hotmail.com>
2023-08-01 22:20:53 -0300

    criei roteamento aula 18

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,13 @@ import { FirstComponentComponent } from './components/first-component/first-comp
 import { ListRenderComponent } from './components/list-render/list-render.component';
 import { RouterComponent } from './components/router/router.component';
 import { AppComponent } from './app.component';
+import { Aula18Component } from './components/aula18/aula18.component';
 
 const routes: Routes = [
   {path:"aula01", component:AppComponent},
   {path:"aula02", component:AppComponent},
   {path:"aula03", component:AppComponent},
+  {path:"aula18", component:Aula18Component},
   {path:"", component: FirstComponentComponent},
   {path:"list", component:ListRenderComponent},
   {path:"router", component:RouterComponent}
